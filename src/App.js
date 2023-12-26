bf0a5509ca152936c96bea73c215af8cb114fb1e
oussama <oussema.lamine20@gmail.com>
2023-12-26 16:11:00 +0100

    Initial commitù

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Button, Navbar, Nav, Container, Row, Col, Card } from 'react-bootstrap';  // Add these imports
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        {/* Navbar */}
+        <Navbar bg="dark" variant="dark">
+          <Navbar.Brand href="#home">React Bootstrap Example</Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#contact">Contact</Nav.Link>
+          </Nav>
+        </Navbar>
+
+        {/* Heading */}
+        <h1>Welcome to React Bootstrap</h1>
+
+        {/* Cards */}
+        <Container>
+          <Row>
+            <Col>
+              <Card>
+                <Card.Img variant="top" src="https://via.placeholder.com/150" />
+                <Card.Body>
+                  <Card.Title>Card 1</Card.Title>
+                  <Card.Text>
+                    This is a sample card with some quick example text to build on the card title
+                    and make up the bulk of the card's content.
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col>
+              <Card>
+                <Card.Img variant="top" src="https://via.placeholder.com/150" />
+                <Card.Body>
+                  <Card.Title>Card 2</Card.Title>
+                  <Card.Text>
+                    This is a sample card with some quick example text to build on the card title
+                    and make up the bulk of the card's content.
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col>
+              <Card>
+                <Card.Img variant="top" src="https://via.placeholder.com/150" />
+                <Card.Body>
+                  <Card.Title>Card 3</Card.Title>
+                  <Card.Text>
+                    This is a sample card with some quick example text to build on the card title
+                    and make up the bulk of the card's content.
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </>
   );
 }
 
